1bb160b0086bfa2fc6e3f9863dc9ed7bc4415e7d
Mark.Etzel@gmx.email <Mark.Etzel@gmx.de>
2020-11-10 13:51:28 +0100

    exerciss 1-4

--- classwork.php
+++ classwork.php
@@ -9,9 +9,98 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <title>Classwork</title>
+
+  <!-- Exercise 1 -->
+  <?php
+    $viewer = getenv( "HTTP_USER_AGENT" );
+    $browser = "An unidentified browser";
+    if( preg_match( "/MSIE/i", "$viewer" ) )
+    {
+    $browser = "Internet Explorer" ;
+    }
+    else if( preg_match( "/Netscape/i", "$viewer"  ) )
+    {
+    $browser = "Netscape";
+    }
+    elseif(preg_match('/Chrome/i' , "$viewer"))
+    {
+    $browser = 'Google Chrome';
+    }
+    else  if( preg_match( "/Mozilla/i", "$viewer" ))
+    {
+    $browser = "Mozilla" ;
+    }
+    elseif(preg_match('/Safari/i',"$viewer"))
+    {
+    $browser = 'Apple Safari';
+    }
+    $platform = "An unidentified OS!";
+    if( preg_match( "/Windows/i", "$viewer" ) )
+    {
+    $platform = "Windows!";
+    }
+    else if ( preg_match( "/Linux/i", "$viewer"  ) )
+    {
+    $platform = "Linux!";
+    }
+    else if  ( preg_match( "/Mac/i", "$viewer" ) )
+    {
+    $platform = "Mac!";
+    }
+    echo $viewer;
+    echo("<br> You are using $browser on $platform <br>");
+  ?>
+
   </head>
-  
+
   <body>
+
+  <!-- Exercise 2 -->
+  <form action="classwork.php" method="post">
+    First name: <input type="text" name="first_name">
+    Last name: <input type="text" name="last_name">
+    <input type="submit" name="submit">
+  </form>
+
+  <form action="classwork.php" method="get">
+    Email: <input type="text" name="email">
+    <input type="submit" name="submit2">
+  </form>
+
+  <?php
+    if(isset($_POST['submit'])) {
+        if ($_POST["first_name"] || $_POST["last_name"]) {
+            echo "Welcome" , $_POST["first_name"] . $_POST["last_name"];
+        }
+    }
+
+    if(isset($_GET['submit2'])) {
+        if ($_GET["email"]) {
+            echo $_GET["email"] . "has been added to Spam-newsletter";
+        }
+    }
+
+    // Exercise 3
+    function divide($a, $b) {
+        return $a/$b;
+    }
+    echo divide(10,2) . "<br>";
+
+    // Exercise 4
+    $servername = "localhost";
+    $username = "root5";
+    $password = "";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+
+    mysqli_close($conn);
+  ?>
+
     
     <!-- Optional JavaScript; choose one of the two! -->
 
